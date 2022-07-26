53046b7539f85703e836201f2a248af043b3dc2b
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-07-25 18:25:36 -0700

    add hook for password protection

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
 
 // create our User model
 class User extends Model {}
@@ -44,9 +46,20 @@ User.init(
           }
         }
       },
-  {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
+      {
+        hooks: {
+           // set up beforeCreate lifecycle "hook" functionality
+           async beforeCreate(newUserData) {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+            },
+            //set upbeforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
+    // TABLe CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
     // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
     // don't automatically create createdAt/updatedAt timestamp fields
