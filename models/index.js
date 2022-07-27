476125a3bf06d6bca38680b12306abfa1181a144
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-07-27 14:25:00 -0700

    forgot to change to feature/votes, but added many-to-many relationships with vote model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 
 //create associations (a one-to-many relationship)
@@ -10,7 +11,37 @@ User.hasMany(Post, {
 //create reverse association - a post can belong to one user, but not many users
 Post.belongsTo(User, {
     foreignKey: 'user_id',
-})
+});
+
+//many-to-many relastionship
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+  
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+//directly connect the Post and Vote models, and User to Vote models
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+  
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+  
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+  
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
 
 
-module.exports = { User, Post };
+module.exports = { User, Post, Vote };
