0ffd0cabc8f75adc8ce8475e80a6d0f63f6b1f3a
Robin Ostrowski <104393956+Bin-Ostrowski@users.noreply.github.com>
2022-08-03 14:07:29 -0700

    add loggedIn to be able to view or not view comment form/buttons and login/logout in nav

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -32,28 +32,71 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
+      // serialize the data
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
+      // pass data to template
+      res.render('homepage', { 
+        posts,
+        //can now use loggedIn in {{#if}} statments in handlebars
+        loggedIn: req.session.loggedIn });
+        // Fortunately, variables that are passed to view templates
+        // are automatically passed to the main layout 
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
 });
-  // console.log(req.session);
-  // res.render('homepage', {
-  //   id: 1,
-  //   post_url: 'https://handlebarsjs.com/guide/',
-  //   title: 'Handlebars Docs',
-  //   created_at: new Date(),
-  //   vote_count: 10,
-  //   comments: [{}, {}],
-  //   user: {
-  //     username: 'test_user'
-  //   }
-  // });
+ 
+//render single post page
+router.get('/post/:id', (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
 
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render('single-post', { 
+        post,
+        //can now use loggedIn in {{#if}} statments in handlebars
+        loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
 
 //render login page
 router.get('/login', (req, res) => {
